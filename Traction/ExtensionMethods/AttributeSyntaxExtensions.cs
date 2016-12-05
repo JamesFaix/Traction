9e7136abcd9b420e8e9c7c0c878d93d4e13f4b1d
JamesFaix <jamesthefaix@gmail.com>
2016-12-05 16:11:40 -0500

    Updated AttributeSyntaxExtensions
    Corrected some bugs and revised API a bit

--- Traction/ExtensionMethods/AttributeSyntaxExtensions.cs
+++ Traction/ExtensionMethods/AttributeSyntaxExtensions.cs
@@ -33,44 +33,32 @@ namespace Traction {
         }
 
         #endregion
-
-        public static IEnumerable<AttributeSyntax> ReturnValueAttributes(this MethodDeclarationSyntax node) {
-            if (node == null) throw new ArgumentNullException(nameof(node));
-            //   Debugger.Launch();
-            return node.AttributeLists
-                .Where(IsAttributeListTargetingReturnValue)
-                .SelectMany(list => list.Attributes);
-        }
-
-        private static bool IsAttributeListTargetingReturnValue(AttributeListSyntax attributes) {
-            var specifier = attributes.ChildNodes()
-                .OfType<AttributeTargetSpecifierSyntax>()
-                .FirstOrDefault();
-
-            return specifier?.Identifier.Text == "return";
-        }
-
+        
         #region Has Attribute
 
-        public static bool HasAttribute<TAttribute>(this BaseMethodDeclarationSyntax node, SemanticModel model) where TAttribute : Attribute {
+        public static bool HasAttribute<TAttribute>(this BaseMethodDeclarationSyntax node, SemanticModel model) 
+            where TAttribute : Attribute {
             if (node == null) throw new ArgumentNullException(nameof(node));
             if (model == null) throw new ArgumentNullException(nameof(model));
             return HasAttributeImpl<TAttribute>(node.AllAttributes(), model);
         }
 
-        public static bool HasAttribute<TAttribute>(this ParameterSyntax node, SemanticModel model) where TAttribute : Attribute {
+        public static bool HasAttribute<TAttribute>(this ParameterSyntax node, SemanticModel model) 
+            where TAttribute : Attribute {
             if (node == null) throw new ArgumentNullException(nameof(node));
             if (model == null) throw new ArgumentNullException(nameof(model));
             return HasAttributeImpl<TAttribute>(node.AllAttributes(), model);
         }
 
-        public static bool HasAttribute<TAttribute>(this TypeDeclarationSyntax node, SemanticModel model) where TAttribute : Attribute {
+        public static bool HasAttribute<TAttribute>(this TypeDeclarationSyntax node, SemanticModel model) 
+            where TAttribute : Attribute {
             if (node == null) throw new ArgumentNullException(nameof(node));
             if (model == null) throw new ArgumentNullException(nameof(model));
             return HasAttributeImpl<TAttribute>(node.AllAttributes(), model);
         }
 
-        public static bool HasAttribute<TAttribute>(this PropertyDeclarationSyntax node, SemanticModel model) where TAttribute : Attribute {
+        public static bool HasAttribute<TAttribute>(this PropertyDeclarationSyntax node, SemanticModel model) 
+            where TAttribute : Attribute {
             if (node == null) throw new ArgumentNullException(nameof(node));
             if (model == null) throw new ArgumentNullException(nameof(model));
             return HasAttributeImpl<TAttribute>(node.AllAttributes(), model);
@@ -78,16 +66,36 @@ namespace Traction {
 
         #endregion
 
-        public static bool ContainsAttribute<TAttribute>(this IEnumerable<AttributeSyntax> attributes, SemanticModel model) where TAttribute : Attribute {
-            if (attributes == null) throw new ArgumentNullException(nameof(attributes));
+        public static bool HasReturnValueAttribute<TAttribute>(this MethodDeclarationSyntax node, SemanticModel model)
+            where TAttribute: Attribute {
+            if (node == null) throw new ArgumentNullException(nameof(node));
             if (model == null) throw new ArgumentNullException(nameof(model));
+            var attributes = node.ReturnValueAttributes();
             return HasAttributeImpl<TAttribute>(attributes, model);
         }
 
-        private static bool HasAttributeImpl<TAttribute>(IEnumerable<AttributeSyntax> attributes, SemanticModel model) where TAttribute : Attribute {
-            //Debugger.Launch();
+        private static bool HasAttributeImpl<TAttribute>(IEnumerable<AttributeSyntax> attributes, SemanticModel model) 
+            where TAttribute : Attribute {
+           // Debugger.Launch();
             var symbol = typeof(TAttribute).GetTypeSymbol(model);
-            return attributes.Any(a => symbol.Equals(model.GetTypeInfo(a).Type));
+            var attributeTypes = attributes.Select(a => model.GetTypeInfo(a).Type);
+            return attributeTypes.Any(t => t.Equals(symbol));
         }
+
+        public static IEnumerable<AttributeSyntax> ReturnValueAttributes(this MethodDeclarationSyntax node) {
+            if (node == null) throw new ArgumentNullException(nameof(node));
+            //   Debugger.Launch();
+            return node.AttributeLists
+                .Where(list => list.SpecifierText() == "return")
+                .SelectMany(list => list.Attributes);
+        }
+
+        public static string SpecifierText(this AttributeListSyntax attributes) {
+            if (attributes == null) throw new ArgumentNullException(nameof(attributes));
+            return attributes.ChildNodes()
+                .OfType<AttributeTargetSpecifierSyntax>()
+                .FirstOrDefault()
+                ?.Identifier.Text;
+        }        
     }
 }
