3fa138499d69022ad3ce5526ebda1a5307c06016
JamesFaix <jamesthefaix@gmail.com>
2016-12-05 16:15:07 -0500

    Adjusted NonNullAttribute
    Added globally qualified type names to injected code, fixed curly brace
    escape issue, removed complicated unique local var name generation logic
    and replaced with nested curly-brace block.

--- Traction/Rewriters/NonNullAttributeRewriter.cs
+++ Traction/Rewriters/NonNullAttributeRewriter.cs
@@ -3,30 +3,31 @@ using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics;
 
 namespace Traction {
 
     class NonNullAttributeReWriter : ContractRewriterBase<NonNullAttribute> {
 
-        public NonNullAttributeReWriter(SemanticModel model) : base(model) {
+        public NonNullAttributeReWriter(SemanticModel model, ICompileContext context) : base(model, context) {
         }
 
         private const string preconditionTemplate =
-            @"if (Object.Equals({0}, null)) 
-                  throw new ArgumentNullException(nameof({0}));";
+            @"if (global::System.Object.Equals({0}, null)) 
+                  throw new global::System.ArgumentNullException(nameof({0}));";
 
         private const string postconditionTemplate =
-            @"{
+            @"{{
                   var {0} = {1};
-                  if (Object.Equals({0}, null))
-                      throw new ReturnValueException(""Return value cannot be null."");
+                  if (global::System.Object.Equals({0}, null))
+                      throw new global::Traction.ReturnValueException(""Return value cannot be null."");
                   return {0};
-              }";
+              }}";
 
         protected override SyntaxList<StatementSyntax> CreatePrecondition(TypeInfo type, string parameterName) {
             if (parameterName == null) throw new ArgumentNullException(nameof(parameterName));
 
-           // System.Diagnostics.Debugger.Launch();
+           //Debugger.Launch();
             if (!type.Type.IsReferenceType) throw new NotSupportedException(
                 $"{nameof(NonNullAttribute)} can only be applied to reference types.");
 
@@ -39,7 +40,7 @@ namespace Traction {
         protected override SyntaxList<StatementSyntax> CreatePostcondition(TypeInfo returnType, ReturnStatementSyntax node) {
             if (node == null) throw new ArgumentNullException(nameof(node));
 
-            System.Diagnostics.Debugger.Launch();
+            //Debugger.Launch();
 
             if (!returnType.Type.IsReferenceType) throw new NotSupportedException(
                 $"{nameof(NonNullAttribute)} can only be applied to reference types.");
@@ -49,17 +50,17 @@ namespace Traction {
             if (returnedExpression == null) throw new NotSupportedException(
                 $"{nameof(NonNullAttribute)} cannot be applied to the return value of methods with no return type.");
 
-            //Find all symbol names accessible from the defining type (excessive but thorough)
-            var typeDeclaration = node.FirstAncestorOrSelf<TypeDeclarationSyntax>();
-            var typeInfo = model.GetTypeInfo(typeDeclaration);
-            var typeSymbol = typeInfo.Type;
-            var illegalNames = model.LookupSymbols(0, typeSymbol).Select(symbol => symbol.Name);
+            ////Find all symbol names accessible from the defining type (excessive but thorough)
+            //var typeDeclaration = node.FirstAncestorOrSelf<TypeDeclarationSyntax>();
+            //var typeInfo = model.GetTypeInfo(typeDeclaration);
+            //var typeSymbol = typeInfo.Type;
+            //var illegalNames = model.LookupSymbols(0, typeSymbol).Select(symbol => symbol.Name);
 
             //Call the temporary var "result", but prepend underscores until there is no name conflict
             var tempVariableName = "result";
-            while (illegalNames.Contains(tempVariableName)) {
-                tempVariableName = "_" + tempVariableName;
-            }
+            //while (illegalNames.Contains(tempVariableName)) {
+            //    tempVariableName = "_" + tempVariableName;
+            //}
 
             var text = string.Format(postconditionTemplate, tempVariableName, returnedExpression);
             var statement = SyntaxFactory.ParseStatement(text);
