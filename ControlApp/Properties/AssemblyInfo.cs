5aec5acba59a6a94d37317d9d600f838581e8216
JamesFaix <jamesthefaix@gmail.com>
2016-12-05 11:54:41 -0500

    Updated assembly infos
    Assembly info files still had old project names

--- ControlApp/Properties/AssemblyInfo.cs
+++ ControlApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ControlApp2")]
+[assembly: AssemblyTitle("Traction.ControlApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ControlApp2")]
+[assembly: AssemblyProduct("Traction.ControlApp")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
