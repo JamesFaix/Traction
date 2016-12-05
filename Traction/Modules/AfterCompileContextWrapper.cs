109ffead5d3b4dd1e2c3995ba3723d66af6a110b
JamesFaix <jamesthefaix@gmail.com>
2016-12-05 16:10:20 -0500

    Create ICompileContext interface
    Created ICompileContext interface to abstract commonalities between
    StackExchange.Precompilations BeforeCompileContext and
    AfterCompileContext.

---
+++ Traction/Modules/AfterCompileContextWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using StackExchange.Precompilation;
+
+namespace Traction {
+
+    class AfterCompileContextWrapper : ICompileContext {
+
+        public AfterCompileContextWrapper(AfterCompileContext context) {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            this.context = context;
+        }
+
+        private readonly AfterCompileContext context;
+
+        public CSharpCompilation Compilation {
+            get { return context.Compilation; }
+            set { context.Compilation = value; }
+        }
+
+        public CSharpCommandLineArguments Arguments {
+            get { return context.Arguments; }
+            set { context.Arguments = value; }
+        }
+
+        public IList<Diagnostic> Diagnostics {
+            get { return context.Diagnostics; }
+            set { context.Diagnostics = value; }
+        }
+    }
+}
---
+++ Traction/Modules/ICompileContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Traction {
+
+    public interface ICompileContext {
+
+        CSharpCompilation Compilation { get; set; }
+        CSharpCommandLineArguments Arguments { get; set; }
+        IList<Diagnostic> Diagnostics { get; set; }
+    }
+}
