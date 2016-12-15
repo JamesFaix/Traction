3fa84dc3ee28f9a567e619d4c673d4ea01b6a7e5
JamesFaix <jamesthefaix@gmail.com>
2016-12-14 21:55:34 -0500

    Create valid type tests for all contracts

--- Traction.CompileTimeDiagnosticsTests/ValidTypeTests.cs
+++ Traction.CompileTimeDiagnosticsTests/ValidTypeTests.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using NUnit.Framework;
-using static Traction.DiagnosticsTests.TestCaseFactory;
 
 namespace Traction.DiagnosticsTests {
 
@@ -26,57 +25,162 @@ namespace Traction.DiagnosticsTests {
             };
         }
 
+        private static TestCaseData PreconditionCase(string typeName, string attributeName, bool shouldHaveDiagnostic) {
+            return new TestCaseData((CompilationGenerator)(() =>
+                    CompilationFactory.SingleClassCompilation(
+                        MemberFactory.MethodWithPrecondition(typeName, attributeName))))
+                .Returns(new DiagnosticSummary {
+                    Count = shouldHaveDiagnostic ? 1 : 0,
+                    FirstMessage = shouldHaveDiagnostic ? "Traction: Invalid contract attribute usage" : null
+                })
+                .SetName($"{attributeName}_CanOnlyBePlacedOnParametersWithValidTypes" +
+                    $"_{typeName}{(shouldHaveDiagnostic ? "Fails" : "Passes")}");
+        }
+
+        private static TestCaseData PostconditionCase(string typeName, string attributeName, bool shouldHaveDiagnostic) {
+            return new TestCaseData((CompilationGenerator)(() =>
+                     CompilationFactory.SingleClassCompilation(
+                         MemberFactory.MethodWithPostcondition(typeName, attributeName))))
+                 .Returns(new DiagnosticSummary {
+                     Count = shouldHaveDiagnostic ? 1 : 0,
+                     FirstMessage = shouldHaveDiagnostic ? "Traction: Invalid contract attribute usage" : null
+                 })
+                 .SetName($"{attributeName}_CanOnlyBePlacedOnReturnValuesWithValidTypes" +
+                     $"_{typeName}{(shouldHaveDiagnostic ? "Fails" : "Passes")}");
+        }
+
         private static IEnumerable<TestCaseData> AllCases =>
             NonNullCases
-            .Concat(NonDefaultCases);
+            .Concat(NonDefaultCases)
+            .Concat(NonEmptyCases)
+            .Concat(PositiveCases);
 
         private static IEnumerable<TestCaseData> NonNullCases {
             get {
                 var name = "NonNull";
-                yield return CreatePreconditionInvalidTypeTestCase("string", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("string", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("object", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("object", name, false);
+                //Reference types OK
+                yield return PreconditionCase("string", name, false);
+                yield return PostconditionCase("string", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("IDisposable", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("IDisposable", name, false);
+                yield return PreconditionCase("object", name, false);
+                yield return PostconditionCase("object", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("int", name, true);
-                yield return CreatePostconditionInvalidTypeTestCase("int", name, true);
+                yield return PreconditionCase("IDisposable", name, false);
+                yield return PostconditionCase("IDisposable", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("int?", name, true);
-                yield return CreatePostconditionInvalidTypeTestCase("int?", name, true);
+                yield return PreconditionCase("Action", name, false);
+                yield return PostconditionCase("Action", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("DateTime", name, true);
-                yield return CreatePostconditionInvalidTypeTestCase("DateTime", name, true);
+                //Value types (even nullable) create diagnostic
+                yield return PreconditionCase("int", name, true);
+                yield return PostconditionCase("int", name, true);
+
+                yield return PreconditionCase("int?", name, true);
+                yield return PostconditionCase("int?", name, true);
+
+                yield return PreconditionCase("DateTime", name, true);
+                yield return PostconditionCase("DateTime", name, true);
             }
         }
 
         private static IEnumerable<TestCaseData> NonDefaultCases {
             get {
                 var name = "NonDefault";
-                yield return CreatePreconditionInvalidTypeTestCase("string", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("string", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("object", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("object", name, false);
+                //All types OK
+                yield return PreconditionCase("string", name, false);
+                yield return PostconditionCase("string", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("IDisposable", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("IDisposable", name, false);
+                yield return PreconditionCase("object", name, false);
+                yield return PostconditionCase("object", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("Action", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("Action", name, false);
+                yield return PreconditionCase("IDisposable", name, false);
+                yield return PostconditionCase("IDisposable", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("int", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("int", name, false);
+                yield return PreconditionCase("Action", name, false);
+                yield return PostconditionCase("Action", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("int?", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("int?", name, false);
+                yield return PreconditionCase("int", name, false);
+                yield return PostconditionCase("int", name, false);
 
-                yield return CreatePreconditionInvalidTypeTestCase("DateTime", name, false);
-                yield return CreatePostconditionInvalidTypeTestCase("DateTime", name, false);
+                yield return PreconditionCase("int?", name, false);
+                yield return PostconditionCase("int?", name, false);
+
+                yield return PreconditionCase("DateTime", name, false);
+                yield return PostconditionCase("DateTime", name, false);
             }
         }
+
+        private static IEnumerable<TestCaseData> NonEmptyCases {
+            get {
+                var name = "NonEmpty";
+
+                //IEnumerable<T> types OK
+                yield return PreconditionCase("string", name, false);
+                yield return PostconditionCase("string", name, false);
+
+                yield return PreconditionCase("int[]", name, false);
+                yield return PostconditionCase("int[]", name, false);
+
+                yield return PreconditionCase("Dictionary<int, string>", name, false);
+                yield return PostconditionCase("Dictionary<int, string>", name, false);
+
+                //Non-generic enumerable types not OK
+                yield return PreconditionCase("ArrayList", name, true);
+                yield return PostconditionCase("ArrayList", name, true);
+                
+                //Non-enumerable types not OK
+                yield return PreconditionCase("object", name, true);
+                yield return PostconditionCase("object", name, true);
+
+                yield return PreconditionCase("IDisposable", name, true);
+                yield return PostconditionCase("IDisposable", name, true);
+
+                yield return PreconditionCase("Action", name, true);
+                yield return PostconditionCase("Action", name, true);
+
+                yield return PreconditionCase("int", name, true);
+                yield return PostconditionCase("int", name, true);
+
+                yield return PreconditionCase("int?", name, true);
+                yield return PostconditionCase("int?", name, true);
+
+                yield return PreconditionCase("DateTime", name, true);
+                yield return PostconditionCase("DateTime", name, true);
+            }
+        }
+
+        //No extra tests for Negative, NonPositive and NonNegative because the type filtering is done by their base class
+        private static IEnumerable<TestCaseData> PositiveCases {
+            get {
+                var name = "Positive";
+
+                //Value types (even nullable) OK
+                yield return PreconditionCase("int", name, false);
+                yield return PostconditionCase("int", name, false);
+
+                yield return PreconditionCase("int?", name, false);
+                yield return PostconditionCase("int?", name, false);
+
+                yield return PreconditionCase("DateTime", name, false);
+                yield return PostconditionCase("DateTime", name, false);
+
+                //Reference types not OK
+                yield return PreconditionCase("string", name, true);
+                yield return PostconditionCase("string", name, true);
+
+                yield return PreconditionCase("object", name, true);
+                yield return PostconditionCase("object", name, true);
+
+                yield return PreconditionCase("IDisposable", name, true);
+                yield return PostconditionCase("IDisposable", name, true);
+
+                yield return PreconditionCase("Action", name, true);
+                yield return PostconditionCase("Action", name, true);
+
+            }
+        }
+
     }
 }
