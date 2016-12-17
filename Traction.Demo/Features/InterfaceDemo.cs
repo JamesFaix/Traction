b094d37adf55bc825c6af0027956dc7d23bfa4ab
JamesFaix <jamesthefaix@gmail.com>
2016-12-17 17:05:32 -0500

    Created unit tests for class inheritance in demo

---
+++ Traction.Demo/Features/InterfaceDemo.cs
@@ -0,0 +1,13 @@
+﻿namespace Traction.Demo {
+
+    public interface InterfaceDemo {
+
+        void PreconditionMethod([NonNull] string text);
+
+        [return: NonNull]
+        string Postconditionmethod();
+
+        [NonNull]
+        string Name { get; set; }
+    }
+}
---
+++ Traction.DemoTests/Features/DerivedClassTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics.Contracts;
+using NUnit.Framework;
+
+namespace Traction.Demo.Tests {
+
+    [TestFixture]
+    class DerivedClassTest {
+
+        private DerivedClassDemo GetConsumer() => new DerivedClassDemo();
+
+        [Test]
+        public void DerivedClass_PreconditionMethod_DoesNotThrowIfContractMet() {
+            var consumer = GetConsumer();
+
+            Assert.DoesNotThrow(() => {
+                consumer.PreconditionMethod("test");
+            });
+        }
+
+        [Test]
+        public void DerivedClass_PreconditionMethod_ThrowsIfContractBroken() {
+            var consumer = GetConsumer();
+
+            Assert.Throws<PreconditionException>(() => {
+                consumer.PreconditionMethod(null);
+            });
+        }
+
+        [Test]
+        public void DerivedClass_PostconditionMethod_DoesNotThrowIfContractMet() {
+            var consumer = GetConsumer();
+
+            Assert.DoesNotThrow(() => {
+                consumer.PostconditionMethod("test");
+            });
+        }
+
+        [Test]
+        public void DerivedClass_PostconditionMethod_ThrowsIfContractBroken() {
+            var consumer = GetConsumer();
+
+            Assert.Throws<PostconditionException>(() => {
+                consumer.PostconditionMethod(null);
+            });
+        }
+
+        [Test]
+        public void DerivedClass_PreconditionProperty_DoesNotThrowIfContractMet() {
+            var consumer = GetConsumer();
+
+            Assert.DoesNotThrow(() => {
+                consumer.Property = "test";
+            });
+        }
+
+        [Test]
+        public void DerivedClass_PreconditionProperty_ThrowsIfContractBroken() {
+            var consumer = GetConsumer();
+
+            Assert.Throws<PreconditionException>(() => {
+                consumer.Property = null;
+            });
+        }
+
+        [Test]
+        public void DerivedClass_PostconditionProperty_DoesNotThrowIfContractMet() {
+            var consumer = GetConsumer();
+            consumer._property = "test";
+
+            Assert.DoesNotThrow(() => {
+                var x = consumer.Property;
+            });
+        }
+
+        [Test]
+        public void DerivedClass_PostconditionProperty_ThrowsIfContractBroken() {
+            var consumer = GetConsumer();
+
+            Assert.Throws<PostconditionException>(() => {
+                var x = consumer.Property;
+            });
+        }
+
+    }
+}
