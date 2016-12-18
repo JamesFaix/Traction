3ac99b415111635e6c16133c0c0027eb92543eb6
JamesFaix <jamesthefaix@gmail.com>
2016-12-18 17:27:14 -0500

    Added tests for interface inheritance

--- Traction.Demo/Features/InterfaceDemo.cs
+++ Traction.Demo/Features/InterfaceDemo.cs
@@ -1,13 +1,45 @@
 ﻿namespace Traction.Demo {
 
-    public interface InterfaceDemo {
+    public interface IDemo {
 
         void PreconditionMethod([NonNull] string text);
 
         [return: NonNull]
-        string Postconditionmethod();
+        string PostconditionMethod(string text);
 
         [NonNull]
-        string Name { get; set; }
+        string Property { get; set; }
+    }
+
+    public class ImplicitInterfaceDemo : IDemo {
+        public void PreconditionMethod(string text) {
+
+        }
+
+        public string PostconditionMethod(string text) {
+            return text;
+        }
+
+        public string _property;
+        public string Property {
+            get { return _property; }
+            set { _property = value; }
+        }
+    }
+
+    public class ExplicitInterfaceDemo : IDemo {
+        void IDemo.PreconditionMethod(string text) {
+
+        }
+
+        string IDemo.PostconditionMethod(string text) {
+            return text;
+        }
+
+        public string _property;
+        string IDemo.Property {
+            get { return _property; }
+            set { _property = value; }
+        }
     }
 }
---
+++ Traction.DemoTests/Features/InterfaceTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Traction.Demo;
+
+namespace Traction.DemoTests {
+
+    [TestFixture]
+    class InterfaceTest {
+
+        private IDemo GetImplicitDemo() => new ImplicitInterfaceDemo();
+        private IDemo GetExplicitDemo() => new ExplicitInterfaceDemo();
+
+        [Test, TestCaseSource(nameof(AllCases))]
+        public void Test(IDemo demo, Action<IDemo> action, Type exceptionType) {
+            CustomAssert.Throws(exceptionType, () => action(demo));
+        }
+
+        private static IEnumerable<TestCaseData> AllCases {
+            get {
+                Action<IDemo> action;
+
+                action = demo => { demo.PreconditionMethod("test"); };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, null)
+                    .SetName($"ImplicitInterfaceImplementation_PreconditionMethod_DoesNotThrowIfContractMet");
+
+                action = demo => { demo.PreconditionMethod(null); };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, typeof(PreconditionException))
+                    .SetName($"ImplicitInterfaceImplementation_PreconditionMethod_ThrowsIfContractBroken");
+
+                action = demo => { demo.PostconditionMethod("test"); };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, null)
+                    .SetName($"ImplicitInterfaceImplementation_PostconditionMethod_DoesNotThrowIfContractMet");
+
+                action = demo => { demo.PostconditionMethod(null); };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, typeof(PostconditionException))
+                    .SetName($"ImplicitInterfaceImplementation_PostconditionMethod_ThrowsIfContractBroken");
+
+                action = demo => { demo.Property = "test"; };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, null)
+                    .SetName($"ImplicitInterfaceImplementation_PreconditionProperty_DoesNotThrowIfContractMet");
+
+                action = demo => { demo.Property = null; };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, typeof(PreconditionException))
+                    .SetName($"ImplicitInterfaceImplementation_PreconditionProperty_ThrowsIfContractBroken");
+
+                action = demo => {
+                    ((dynamic)(demo))._property = "test";
+                    var x = demo.Property;
+                };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, null)
+                    .SetName($"ImplicitInterfaceImplementation_PostconditionProperty_DoesNotThrowIfContractMet");
+
+                action = demo => { var x = demo.Property; };
+                yield return new TestCaseData(new ImplicitInterfaceDemo(), action, typeof(PostconditionException))
+                    .SetName($"ImplicitInterfaceImplementation_PostconditionProperty_ThrowsIfContractBroken");
+
+
+                action = demo => { demo.PreconditionMethod("test"); };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, null)
+                    .SetName($"ExplicitInterfaceImplementation_PreconditionMethod_DoesNotThrowIfContractMet");
+
+                action = demo => { demo.PreconditionMethod(null); };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, typeof(PreconditionException))
+                    .SetName($"ExplicitInterfaceImplementation_PreconditionMethod_ThrowsIfContractBroken");
+
+                action = demo => { demo.PostconditionMethod("test"); };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, null)
+                    .SetName($"ExplicitInterfaceImplementation_PostconditionMethod_DoesNotThrowIfContractMet");
+
+                action = demo => { demo.PostconditionMethod(null); };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, typeof(PostconditionException))
+                    .SetName($"ExplicitInterfaceImplementation_PostconditionMethod_ThrowsIfContractBroken");
+
+                action = demo => { demo.Property = "test"; };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, null)
+                    .SetName($"ExplicitInterfaceImplementation_PreconditionProperty_DoesNotThrowIfContractMet");
+
+                action = demo => { demo.Property = null; };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, typeof(PreconditionException))
+                    .SetName($"ExplicitInterfaceImplementation_PreconditionProperty_ThrowsIfContractBroken");
+
+                action = demo => {
+                    ((dynamic)(demo))._property = "test";
+                    var x = demo.Property;
+                };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, null)
+                    .SetName($"ExplicitInterfaceImplementation_PostconditionProperty_DoesNotThrowIfContractMet");
+
+                action = demo => { var x = demo.Property; };
+                yield return new TestCaseData(new ExplicitInterfaceDemo(), action, typeof(PostconditionException))
+                    .SetName($"ExplicitInterfaceImplementation_PostconditionProperty_ThrowsIfContractBroken");
+            }
+        }
+    }
+}
