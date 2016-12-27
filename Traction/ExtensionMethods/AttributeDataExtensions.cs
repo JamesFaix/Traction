fe864625e94a2e24375b9425d2e8a37183294dbb
JamesFaix <jamesthefaix@gmail.com>
2016-12-27 10:17:59 -0500

    Created new extension methods for finding contracts on elements

---
+++ Traction/ExtensionMethods/AttributeDataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+
+namespace Traction {
+
+    static class AttributeDataExtensions {
+
+        public static bool Extends<TAttrbute>(this AttributeData @this, SemanticModel model) {
+            if (@this == null) throw new ArgumentNullException(nameof(@this));
+            if (model == null) throw new ArgumentNullException(nameof(model));
+
+            var symbol1 = typeof(TAttrbute).GetTypeSymbol(model);
+            var symbol2 = @this.AttributeClass;
+            return symbol2.Ancestors().Contains(symbol1);
+        }
+    }
+}
